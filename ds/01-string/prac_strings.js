0d7b77cdc89eb4a7c1830563313615c39729d3f5
SureshKuchana <kuchana123.suresh@gmail.com>
2022-04-27 20:45:20 +0530

    added anagram

--- ds/01-string/prac_strings.js
+++ ds/01-string/prac_strings.js
@@ -1,9 +1,13 @@
+////////////////////////////////////////////////////////////
+///////////  Reverse of the string      ////////////////////
+////////////////////////////////////////////////////////////
+
 function reverseOfString1(str) {
   let res = "";
   for (let index = str.length - 1; index >= 0; index--) {
     res += str[index];
   }
-  console.log(" reverse of String ", res);
+  return res;
 }
 
 function reverseOfString2(str) {
@@ -11,11 +15,47 @@ function reverseOfString2(str) {
   let strPosition = str.length - 1;
   while (strPosition >= 0) {
     resStr += str[strPosition];
-    strPosition - 1;
+    strPosition -= 1;
   }
-  console.log(" reverse of String ", resStr);
+  return resStr;
 }
 
-reverseOfString1("hello Suresh");
-reverseOfString1("string");
-reverseOfString2("Koochana");
+console.log(reverseOfString1("hello Suresh"));
+console.log(reverseOfString1("string"));
+console.log(reverseOfString2("Koochana"));
+
+////////////////////////////////////////////////////////////
+///////////  Anagram is a word phrase   ////////////////////
+////////////////////////////////////////////////////////////
+
+function anagram(str1, str2) {
+  let objectRes = {};
+
+  // if length of two string's is same then return true string
+  if (str1 === str2) return true;
+
+  // if length of two string's is not same then return false string
+  if (str1.length !== str2.length) return false;
+
+  // looping through each character's of the str1,
+  // checking charCodeAt of current character, substracting with (a ASCII) - 97,
+  // then storing the ASCII res as a key to track the character for later validation,
+  // for now assinging value to 1
+  // finally adding the key-value pair into the object
+  for (let i = 0; i < str1.length; i++) {
+    let char = str1.charCodeAt[i] - 97;
+    objectRes[char] = (objectRes[char] || 0) + 1;
+  }
+
+  for (let j = 0; j < str2.length; j++) {
+    let char = str2.charCodeAt[j] - 97;
+    if (!objectRes[char]) {
+      return false;
+    }
+    objectRes[char]--;
+  }
+  return true;
+}
+
+console.log(anagram("abc", "cba"));
+console.log(anagram("listen", "slient"));
