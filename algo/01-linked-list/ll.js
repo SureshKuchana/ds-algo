29d0418039bb8020c5443f1251e8d9f207983674
SureshKuchana <kuchana123.suresh@gmail.com>
2022-10-25 13:43:40 +0530

    removed algo folder

--- algo/01-linked-list/ll.js
+++
@@ -1,6 +0,0 @@
-const LinkedList = require("./LinkedList");
-const ll = LinkedList.fromValues("prepend", 10);
-const ll1 = LinkedList.fromValues("append", 20, 30);
-
-console.log(ll);
-console.log(ll1);
